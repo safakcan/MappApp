bbd060082191c2594a7bc70f6e486ba6e412545b
Safak Can Bas <safak.bas@colendi.com>
2020-07-01 20:45:01 +0300

    Initial Commit

---
+++ MapApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MapApp
+//
+//  Created by Safak Can Bas on 1.07.2020.
+//  Copyright © 2020 canbas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
